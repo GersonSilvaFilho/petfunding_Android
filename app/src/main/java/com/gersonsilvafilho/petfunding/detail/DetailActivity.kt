88eb5bf21a31b640db593aa748c7f149079be849
Gerson Luis Ferreira da Silva Filho <silvafilho.gerson@gmail.com>
2017-05-05 00:39:00 -0300

    Adding tab layout and view pager with fragments for detail view.

--- app/src/main/java/com/gersonsilvafilho/petfunding/detail/DetailActivity.kt
+++ app/src/main/java/com/gersonsilvafilho/petfunding/detail/DetailActivity.kt
@@ -3,9 +3,16 @@ package com.gersonsilvafilho.petfunding.detail
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import com.gersonsilvafilho.petfunding.R
+import kotlinx.android.synthetic.main.activity_detail.*
+import java.util.*
+
 
 class DetailActivity : AppCompatActivity() {
 
@@ -19,6 +26,39 @@ class DetailActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }
+        setupViewPager(viewpager)
+        tabs.setupWithViewPager(viewpager)
+    }
+
+    private fun setupViewPager(viewPager: ViewPager) {
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(AboutFragment(), "Info")
+        adapter.addFragment(InfoFragment(), "Dados")
+        adapter.addFragment(StatusFragment(), "Condição")
+        adapter.addFragment(ContactFragment(), "Contato")
+        viewPager.adapter = adapter
+    }
+
+    internal inner class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
+        private val mFragmentList = ArrayList<Fragment>()
+        private val mFragmentTitleList = ArrayList<String>()
+
+        override fun getItem(position: Int): Fragment {
+            return mFragmentList.get(position)
+        }
+
+        override fun getCount(): Int {
+            return mFragmentList.size
+        }
+
+        fun addFragment(fragment: Fragment, title: String) {
+            mFragmentList.add(fragment)
+            mFragmentTitleList.add(title)
+        }
+
+        override fun getPageTitle(position: Int): CharSequence {
+            return mFragmentTitleList.get(position)
         }
     }
 }
