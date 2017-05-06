63afa6fa42a47217b7a5fc5a53ce704bc45d7e1f
Gerson Luis Ferreira da Silva Filho <silvafilho.gerson@gmail.com>
2017-05-06 00:29:55 -0300

    Adding rounded image dependency. Adding AddPetActivity and first fragment to add images, name and description.

---
+++ app/src/main/java/com/gersonsilvafilho/petfunding/add_pet/AddPetActivity.kt
@@ -0,0 +1,65 @@
+package com.gersonsilvafilho.petfunding.add_pet
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.widget.TextView
+import com.gersonsilvafilho.petfunding.R
+import com.gersonsilvafilho.petfunding.detail.ContactFragment
+import com.gersonsilvafilho.petfunding.detail.InfoFragment
+import com.gersonsilvafilho.petfunding.detail.StatusFragment
+import kotlinx.android.synthetic.main.activity_detail.*
+import java.util.*
+
+class AddPetActivity : AppCompatActivity() {
+
+    private var mTextMessage: TextView? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_pet)
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        setupViewPager(viewpager)
+        tabs.setupWithViewPager(viewpager)
+    }
+
+    private fun setupViewPager(viewPager: ViewPager) {
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(AboutAddFragment(), "Info")
+        adapter.addFragment(InfoFragment(), "Dados")
+        adapter.addFragment(StatusFragment(), "Condição")
+        adapter.addFragment(ContactFragment(), "Contato")
+        viewPager.adapter = adapter
+    }
+
+    internal inner class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
+        private val mFragmentList = ArrayList<Fragment>()
+        private val mFragmentTitleList = ArrayList<String>()
+
+        override fun getItem(position: Int): Fragment {
+            return mFragmentList.get(position)
+        }
+
+        override fun getCount(): Int {
+            return mFragmentList.size
+        }
+
+        fun addFragment(fragment: Fragment, title: String) {
+            mFragmentList.add(fragment)
+            mFragmentTitleList.add(title)
+        }
+
+        override fun getPageTitle(position: Int): CharSequence {
+            return mFragmentTitleList.get(position)
+        }
+    }
+
+}
